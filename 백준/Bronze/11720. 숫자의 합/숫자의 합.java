6fe82b227c425cd32b18703b5b24312d0181580d
Hae-in <hilee00002012@gmail.com>
2025-09-23 01:04:06 +0900

    Update 숫자의 합.java

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -14,3 +14,20 @@ public class Main {
         System.out.println(sum);
     }
 }
+
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String numbers = sc.next();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += numbers.charAt(i) - '0';
+        }
+        System.out.println(sum);
+    }
+}
+*/
