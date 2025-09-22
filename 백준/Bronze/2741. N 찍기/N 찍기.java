93ec7d25746f945a6d03967b04caef6ec69ef489
Hae-in <hilee00002012@gmail.com>
2025-09-23 01:20:25 +0900

    [Bronze V] Title: N 찍기, Time: 176 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
-            System.out.println(i);
+            sb.append(i).append('\n');
         }
+        System.out.print(sb.toString());
     }
 }
