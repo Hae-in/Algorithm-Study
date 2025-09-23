5da3e9dfef13bdab90870e25db713b5633386c94
Hae-in <hilee00002012@gmail.com>
2025-09-24 03:20:14 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.print("( 0 )\"\"\"\\\n");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
