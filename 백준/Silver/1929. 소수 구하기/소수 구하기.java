a1ad72aa697573fc9d94c0c2c0d594144560f294
Hae-in <hilee00002012@gmail.com>
2025-09-24 02:18:49 +0900

    [Silver III] Title: 소수 구하기, Time: 284 ms, Memory: 24012 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         boolean[] isPrime = new boolean[N + 1];
 
         for (int i = 2; i <= N; i++) {
@@ -18,8 +21,12 @@ public class Main {
         }
         for (int i = M; i <= N; i++) {
             if (isPrime[i]) {
-                System.out.println(i);
+                bw.write(i + "\n");
             }
         }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
