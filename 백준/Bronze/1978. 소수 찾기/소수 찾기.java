a16866692824b11d52965d2cb36283da488d1639
Hae-in <hilee00002012@gmail.com>
2025-09-28 16:14:16 +0900

    [Bronze II] Title: 소수 찾기, Time: 180 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if (isPrime(arr[i])) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
