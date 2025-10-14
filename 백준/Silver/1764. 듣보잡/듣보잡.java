f32f4e97e37a884684d70ab02498d41e38e615df
Hae-in <hilee00002012@gmail.com>
2025-10-14 18:59:14 +0900

    [Silver IV] Title: 듣보잡, Time: 260 ms, Memory: 26176 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,27 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        HashSet<String> list = new HashSet<>();
-        for (int i = 0; i < n; i++) {
+        Set<String> list = new HashSet<>();
+        for (int i = 0; i < N; i++) {
             String s = br.readLine();
             list.add(s);
         }
 
         ArrayList<String> result = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < M; i++) {
             String view = br.readLine();
             if (list.contains(view)) {
                 result.add(view);
